3f32e7a5fd881c3f6a015931c06202b784c42def
Tom Hazledine <tom@tomhazledine.com>
2017-04-06 20:09:02 +0100

    live config url

--- _uncompressed/js/custom/headerScroll.js
+++ _uncompressed/js/custom/headerScroll.js
@@ -1,44 +1,44 @@
-var did_scroll = false;
-var last_scroll_top = 0;
-var min_scroll_threshold = 5;
+// var did_scroll = false;
+// var last_scroll_top = 0;
+// var min_scroll_threshold = 5;
 
-var header = document.getElementById('mainHeader');
-var navbar_height = 100;
-
-
-// When the window scrolls, make a note of it.
-window.addEventListener('scroll', function(e) {
-    did_scroll = true;
-});
-
-// Check scroll-state every 0.25 seconds (a very
-// primitive attempt at a "debounce").
-setInterval(function() {
-    if (did_scroll) {
-        // If the page has scrolled, trigger the scroll function.
-        has_scrolled();
-        did_scroll = false;
-    }
-}, 250);
+// var header = document.getElementById('mainHeader');
+// var navbar_height = 100;
 
 
-function has_scrolled() {
-    var scroll_top = document.body.scrollTop;
+// // When the window scrolls, make a note of it.
+// window.addEventListener('scroll', function(e) {
+//     did_scroll = true;
+// });
 
-    // Only continue if the page has scrolled further than min_scroll_threshold
-    if ( Math.abs( last_scroll_top - scroll_top ) <= min_scroll_threshold ) {
-        return;
-    }
+// // Check scroll-state every 0.25 seconds (a very
+// // primitive attempt at a "debounce").
+// setInterval(function() {
+//     if (did_scroll) {
+//         // If the page has scrolled, trigger the scroll function.
+//         has_scrolled();
+//         did_scroll = false;
+//     }
+// }, 250);
 
-    // If they scrolled down and are past the navbar, add class .tuckedUp.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (scroll_top > navbar_height){
-        // Scroll Down
-        noQuery.addClass(header,'tuckedUp');
-    } else {
-        // Scroll Up
-        noQuery.removeClass(header,'tuckedUp');
-    }
 
-    last_scroll_top = scroll_top;
-}
+// function has_scrolled() {
+//     var scroll_top = document.body.scrollTop;
+
+//     // Only continue if the page has scrolled further than min_scroll_threshold
+//     if ( Math.abs( last_scroll_top - scroll_top ) <= min_scroll_threshold ) {
+//         return;
+//     }
+
+//     // If they scrolled down and are past the navbar, add class .tuckedUp.
+//     // This is necessary so you never see what is "behind" the navbar.
+//     if (scroll_top > navbar_height){
+//         // Scroll Down
+//         noQuery.addClass(header,'tuckedUp');
+//     } else {
+//         // Scroll Up
+//         noQuery.removeClass(header,'tuckedUp');
+//     }
+
+//     last_scroll_top = scroll_top;
+// }
