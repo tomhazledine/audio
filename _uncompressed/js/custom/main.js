35b3f3aa017ee04d99107d8da803436af71aefe5
Tom Hazledine <tom@tomhazledine.com>
2017-04-12 23:54:47 +0100

    working smoothscroll

--- _uncompressed/js/custom/main.js
+++ _uncompressed/js/custom/main.js
@@ -7,20 +7,24 @@ Picobel( { theme: 'basicPlayer' } );
 
 // Trigger Smoothscroll
 
+// Get the menu links
 var internal_anchor_links = document.getElementsByClassName('internal-link');
-
-console.log(internal_anchor_links);
+// Add click event for each menu link.
 for (var i = 0; i < internal_anchor_links.length; i++) {
     internal_anchor_links[i].addEventListener('click',internal_link_click);
-    // console.log(internal_anchor_links[i].attributes.href);
 }
 
+// Menu link click event
 function internal_link_click(e){
+    // Stop the page from automatically jumping to the hash.
     e.preventDefault();
+    // Get the href value.
     var target_href = this.attributes.href.nodeValue;
+    // Strip the "#" from the href (turning it into a useable ID).
     var target_id = target_href.replace("#", "");
+    // Use that ID to get our target element.
     var target = document.getElementById(target_id);
     console.log(target);
     console.log(target.offsetTop);
-    smooth_scroll_to(target, target.offsetTop, 600);
+    smooth_scroll_to(document.body, target.offsetTop, 600);
 }
